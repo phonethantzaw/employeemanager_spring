578f3eca75714cdbb646523ed6f3d29402752c6c
Ko Phone <phonethantzaw@gmail.com>
2021-06-27 03:02:55 +0800

    Initial commit

---
+++ src/main/java/com/kophone/employeemanager/EmployeemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.kophone.employeemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanagerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kophone/employeemanager/service/EmployeeService.java
@@ -0,0 +1,45 @@
+package com.kophone.employeemanager.service;
+
+import com.kophone.employeemanager.exception.UserNotFoundException;
+import com.kophone.employeemanager.model.Employee;
+import com.kophone.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployee() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found "));
+    }
+
+
+    @Transactional
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
+    }
+}
